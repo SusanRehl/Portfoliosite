dfba8cb18a58162f1f051836370c66acf3abda93
Susan Rehl <susanrehl@gmail.com>
2016-10-18 16:15:41 -0500

    Minor changes.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,4 +2,4 @@ var myApp=angular.module( 'myApp', [] );
 
 myApp.controller( 'viewController', [ '$scope', '$http', function( $scope, $http){
 
-}]);  //end addCOntroller
+}]);  //end viewController
--- server/app.js
+++ server/app.js
@@ -4,7 +4,9 @@ var path=require('path');  // sets up paths
 var bodyParser=require('body-parser');  // sets up body-parser for POST method (for admin user feature stretch goal)
 var urlencodedParser=bodyParser.urlencoded( {extended: false} );
 var pg=require('pg');  // sets up postgres database
+
 var connectionString = '';
+
  if(process.env.DATABASE_URL !== undefined) {
      console.log('env connection string');
      connectionString = process.env.DATABASE_URL;
@@ -16,7 +18,7 @@ var connectionString = '';
 
 app.use(bodyParser.json());
 
-
+// Pages //
 app.get( '/', function( req, res ){    // set basic url
   res.sendFile( path.resolve( 'views/index.html' ) );
 });
@@ -45,9 +47,12 @@ app.get( '/blog', function( req, res ){  // makes view.html available
   res.sendFile( path.resolve( 'views/blog.html' ) );
   });
 
-  app.set("port", (process.env.PORT || 8080));
 
-  app.listen(app.get("port"), function(){
+
+// General set up //
+app.set("port", (process.env.PORT || 8080));
+
+app.listen(app.get("port"), function(){
       console.log("Listening on port: ", app.get("port"));
   });
-  app.use(express.static('public'));
+app.use(express.static('public'));
