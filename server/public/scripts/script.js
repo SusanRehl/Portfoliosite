d7c0774dce57a0df2a9dc249a9aedb75f2c2f200
Susan Rehl <susanrehl@gmail.com>
2016-10-28 12:13:27 -0500

    Minor changes

--- server/public/scripts/script.js
+++ server/public/scripts/script.js
@@ -1,4 +1,4 @@
-var myApp=angular.module( 'myApp', ['ngRoute'] );
+var myApp = angular.module("myApp", ["ngRoute"]);
 
 myApp.config(["$routeProvider", function($routeProvider) {   //  , $locationProvider
 	console.log("in scripts in config function");
