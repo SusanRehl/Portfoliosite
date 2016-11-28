b84ff1796b4840730f1c1e3677e224d12c3ddf9f
Susan Rehl <susanrehl@gmail.com>
2016-11-28 16:47:56 -0600

    Refactored site structure and added copy

--- server/public/scripts/script.js
+++ server/public/scripts/script.js
@@ -7,29 +7,25 @@ myApp.config(["$routeProvider", function($routeProvider) {   //  , $locationProv
 			templateUrl: "/views/routes/home.html",
       controller: "homeController"
 		}).
-		when("/art", {
-			templateUrl: "/views/routes/art.html",
-      controller: "artController"
+		when("/coding", {
+			templateUrl: "/views/routes/coding.html",
+      controller: "codingController"
 		}).
-		when("/cats", {
-			templateUrl: "/views/routes/cats.html",
-      controller: "catsController"
+		when("/design", {
+			templateUrl: "/views/routes/design.html",
+      controller: "designController"
+		}).
+    when("/hobbies", {
+			templateUrl: "/views/routes/hobbies.html",
+      controller: "hobbiesController"
+		}).
+		when("/contact", {
+			templateUrl: "/views/routes/contact.html",
+			controller: "contactController"
 		}).
     when("/blog", {
 			templateUrl: "/views/routes/blog.html",
       controller: "blogController"
-		}).
-    when("/diy", {
-			templateUrl: "/views/routes/diy.html",
-      controller: "diyController"
-		}).
-    when("/naturephotos", {
-			templateUrl: "/views/routes/naturephotos.html",
-      controller: "natureController"
-		}).
-    when("/travel", {
-			templateUrl: "/views/routes/travel.html",
-      controller: "travelController"
 		}).
 		otherwise({
 			redirectTo: "/home"
@@ -42,26 +38,22 @@ myApp.controller("homeController", ["$scope", function($scope){
     console.log("Loaded home");
 }]);
 
-myApp.controller("artController", ["$scope", function($scope){
-    console.log("Loaded art");
+myApp.controller("codingController", ["$scope", function($scope){
+    console.log("Loaded coding");
 }]);
 
-myApp.controller("catsController", ["$scope", function($scope){
-    console.log("Loaded cats - hello where are the cats?");
+myApp.controller("designController", ["$scope", function($scope){
+    console.log("Loaded design");
+}]);
+
+myApp.controller("hobbiesController", ["$scope", function($scope){
+    console.log("Loaded hobbies");
+}]);
+
+myApp.controller("contactController", ["$scope", function($scope){
+    console.log("Loaded contact");
 }]);
 
 myApp.controller("blogController", ["$scope", function($scope){
     console.log("Loaded blog");
-}]);
-
-myApp.controller("diyController", ["$scope", function($scope){
-    console.log("Loaded DIY");
-}]);
-
-myApp.controller("natureController", ["$scope", function($scope){
-    console.log("Loaded nature");
-}]);
-
-myApp.controller("travelController", ["$scope", function($scope){
-    console.log("Loaded travel");
 }]);
