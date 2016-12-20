c65129ce2c367755c369a45e214da84285ff6e85
Susan Rehl <susanrehl@gmail.com>
2016-12-20 17:16:42 -0600

    Added resume pdf, photography.sql files

---
+++ photography.sql
@@ -0,0 +1,65 @@
+CREATE TABLE photography (
+	id SERIAL PRIMARY KEY NOT NULL,
+	place VARCHAR (50),
+	image VARCHAR (300),
+	refno VARCHAR (10),
+	text VARCHAR (300)
+	);
+
+
+
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/countryviewfromduomo.jpg','715','The view of Florence from the top of the Duomo (Santa Maria del Fiore)');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/santamarianovellaviewfromduomo.jpg','716','Santa Maria Novella from the top of the Duomo in Florence');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/crestsantamariadelcarmine.jpg','771','One of many family crests (could be the Pitti family) embedded in walls of churches. This one was in the monastery wall at Santa Maria del Carmine');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/pittiart.jpg','793','A room filled with art in the PItti Palace, in Florence.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/palazzovecchio.jpg','845','The Palazzo Vecchio, Renaissance center of government in Florence.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/florencefromboboli.jpg','872','A view of Florence from across the Arno, in the Boboli Gardens which are behind the Pitti Palace.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/davidaccademia.jpg','900','Michelangelos David, in the Accademia museum in Florence.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/sanlorenzointerior.jpg','975','Interior view of San Lorenzo church, designed by Brunelleschi.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/bargellostairway.jpg','1035','The stairway inside the Bargello Museum, which used to be a prison but now holds many amazing and famous medieval and Renaissance masterpieces.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/bargellointerior.jpg','1081','An open colonnade on the upper level of the Bargello.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/duomofrompalazzovecchio.jpg','1108','The Duomo as seen from a slit window going up the many stairs to the top of the Palazzo Vecchios tower.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/santacroce.jpg','1136','Santa Croce church, where many famous people are buried inside the church, including Michelangelo.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/galileotombsantacroce.jpg','1142','The tomb of Galileo, inside Santa Croce.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/santacrocecourtyard.jpg','1170','The courtyard inside of Santa Croce.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/duomonightview.jpg','1206','A view of the Duomo at night, behind the Baptistry and Giottos Tower.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/vinodoor.jpg','1342','A tiny random door in a wall, apparently for a very important wine exchange.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/florencedoor.jpg','1343','One of many beautiful doors in Florence - the detail in every architectural element was stunning.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/pontevecchionightfireworks.jpg','1354','Fireworks on the Arno, near the Ponte Vecchio bridge, at a wedding reception.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Florence','images/florencenightview.jpg','1362','The amazing view of Florence at night from PIazzale Michelangelo, across the Arno.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Siena','images/sienastreet.jpg','1216','A street in the medieval hill town of Siena.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Siena','images/ilcampo.jpg','1226','Il Campo, the main square in Siena, hosts annual horse races.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Siena','images/sienastairs.jpg','1239','The streets included stairs and archways. Directions included \"go left\", \"go right\", \"go up\" and \"go down\".');
+INSERT INTO photography (place,image,refno,text) VALUES ('Siena','images/donatellorondo.jpg','1253','Rondo of Madonna and Child by Donatello in the Siena museum, just one of many famous works.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Siena','images/pisanostatues.jpg','1254','Statues of biblical figures by Pisano, in the late medieval period, originally adorned the cathedrals facade, but are now in the museum.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Siena','images/domeinterior.jpg','1281','One of several domes inside the Siena Cathedral. Every square inch of the church interior was highly decorated.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Siena','images/stjeromebernini.jpg','1297','St. Jerome by Bernini. The Siena cathedral was so reknowned that later artists vied for commissions there.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Siena','images/sandomenico.jpg','1325','View of San Domenico Church in Siena, where St. Catherine of Sienas head is displayed.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Siena','images/madonnastreetshrine.jpg','1328','One of many street shrines to the Madonna.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/pantheon.jpg','1395','The interior of the Pantheon in Rome.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/goatrooftop.jpg','1409','From the rooftop deck of our hotel in Rome, we had an amazing view, including this goat sculpure adorning a nearby roof!');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/rooftopview.jpg','1410','Another rooftop view of Rome, from our hotel rooftop deck.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/largoargentinacat.jpg','1427','A cat lounging in the Largo Argentina area in Rome, an excavated area from classical times, and where Julius Caesar was reputedly assassinated.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/constantinearch.jpg','1492','The view of Constantines Arch from inside the Colosseum in Rome.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/doriapamphilijhall.jpg','1640','A hall in the palace/museum of the Doria Pamphilij family in Rome. This private museum is a part of the ancient familys palace, where they still live.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/vaticanmuseumcrowd.jpg','1696','A view of the crowds inside the Vatican Museums.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/vaticanmuseumstairs.jpg','1818','These stairs, used when exiting the Vatican museums, were designed by Bernini.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/stpetersstatues.jpg','1825','The statues of Christ and the apostles above the pediment of St. Peters in Rome.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/pietastpeters.jpg','1835','The Pieta by Michelangelo, in St. Peters in Rome.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/romesunsetview.jpg','1897','Sunset view of Rome, including St. Peters in the background, from our hotel rooftop deck.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/apolloanddaphneborghese.jpg','1989','Apollo and Daphne by Bernini in the Borghese Museum in Rome.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/piazzanavonnafountain.jpg','2054','Fountain by Bernini at night, in PIazza Navonna, near the Pantheon in Rome.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/romestreet.jpg','2063','A street in Rome: gray cobblestones, no sidewalk, people and small cars everywhere, and trattoria outside dining areas in the street!');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/pantheonnight.jpg','2074','The exterior of the Pantheon at night, in Rome.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/romestreetday.jpg','2110','Another street in Rome, by day.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/borghesepond.jpg','2118','The pond in the Borghese Garden, in Rome.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/tevereview.jpg','2313','The view of St. Peters from the south, down the Tevere River.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/lorso80restaurant.jpg','2354','The LOrso 80 restaurant at night, in Rome.');
+INSERT INTO photography (place,image,refno,text) VALUES ('Rome','images/colosseum.jpg','2253','The Colosseum in Rome.');
+
+
+
+
+
+
+
